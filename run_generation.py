0110fd7de247d956abf673bb515b5766e428a5df
ppjotrek <pichor@student.agh.edu.pl>
2024-05-13 12:33:26 +0200

    css added

--- run_generation.py
+++ run_generation.py
@@ -7,11 +7,6 @@ json_file = open('generation_config.json')
 
 config = json.load(json_file)
 
-print("Config in run_generation.py: ")
-print(config)
-
-print(config['time'])
-
 time = int(config['time'])
 hr = int(config['hr'])
 sr = int(config['sr'])
